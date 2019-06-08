df3175200be8a5a913215c4a7de67b2e260187fb
Nick Elsey <nicholas.elsey@gmail.com>
2019-06-07 22:11:36 -0400

    added extra arguments to make ParseStringToVec to make it more flexible, and did some formatting

--- sct/lib/string/string_utils.cc
+++ sct/lib/string/string_utils.cc
@@ -3,7 +3,7 @@
 namespace sct {
 
 template <>
-void SplitString(const string& str, std::set<string>& cont, char delim) {
+void SplitString(const string &str, std::set<string> &cont, char delim) {
   std::stringstream ss(str);
   string token;
   while (std::getline(ss, token, delim)) {
@@ -11,14 +11,11 @@ void SplitString(const string& str, std::set<string>& cont, char delim) {
   }
 }
 
-template <>
-string MakeString(const string& s) {
-  return s;
-}
+template <> string MakeString(const string &s) { return s; }
 
-string MakeString(const char* chr) { return std::string(chr); }
+string MakeString(const char *chr) { return std::string(chr); }
 
-bool HasEnding(const string& full_string, const string& ending) {
+bool HasEnding(const string &full_string, const string &ending) {
   if (full_string.length() >= ending.length()) {
     return (0 == full_string.compare(full_string.length() - ending.length(),
                                      ending.length(), ending));
@@ -27,8 +24,9 @@ bool HasEnding(const string& full_string, const string& ending) {
   }
 }
 
-bool Consume(string& arg, const string& seq) {
-  if (seq.size() > arg.size()) return false;
+bool Consume(string &arg, const string &seq) {
+  if (seq.size() > arg.size())
+    return false;
   if (arg.compare(0, seq.length(), seq) == 0) {
     arg = string(arg.begin() + seq.length(), arg.end());
     return true;
@@ -36,8 +34,9 @@ bool Consume(string& arg, const string& seq) {
   return false;
 }
 
-bool Consume(string& arg, char c) {
-  if (arg.size() == 0) return false;
+bool Consume(string &arg, char c) {
+  if (arg.size() == 0)
+    return false;
   if (arg[0] == c) {
     arg = string(arg.begin() + 1, arg.end());
     return true;
@@ -45,12 +44,13 @@ bool Consume(string& arg, char c) {
   return false;
 }
 
-string SplitOnNextOccurence(string& arg, const string& seq) {
+string SplitOnNextOccurence(string &arg, const string &seq) {
   size_t loc = arg.find(seq);
-  if (loc == string::npos) return string();
+  if (loc == string::npos)
+    return string();
   string ret = string(arg.begin(), arg.begin() + loc);
   arg = string(arg.begin() + loc + seq.size(), arg.end());
   return ret;
 }
 
-}  // namespace sct
+} // namespace sct
--- sct/lib/string/string_utils.h
+++ sct/lib/string/string_utils.h
@@ -16,7 +16,7 @@ namespace sct {
 // default delimiter is ' '
 
 template <class Container>
-void SplitString(const string& str, Container& cont, char delim = ' ') {
+void SplitString(const string &str, Container &cont, char delim = ' ') {
   std::stringstream ss(str);
   string token;
   while (std::getline(ss, token, delim)) {
@@ -25,61 +25,59 @@ void SplitString(const string& str, Container& cont, char delim = ' ') {
 }
 
 template <>
-void SplitString(const string& str, std::set<string>& cont, char delim);
+void SplitString(const string &str, std::set<string> &cont, char delim);
 
 // template function to make a string out of an arbitrary number
 // of varied inputs, as long as the input has an sstream << overload
 // for instance MakeString("string", 42, 36.5, ClassWith_<<_Overload);
 
-inline void MakeStringInternal(std::stringstream& ss) { return; }
+inline void MakeStringInternal(std::stringstream &ss) { return; }
 
 template <typename T>
-inline void MakeStringInternal(std::stringstream& ss, T& t) {
+inline void MakeStringInternal(std::stringstream &ss, T &t) {
   ss << t;
 }
 
 template <typename T, typename... Args>
-inline void MakeStringInternal(std::stringstream& ss, T& t,
-                               const Args&... args) {
+inline void MakeStringInternal(std::stringstream &ss, T &t,
+                               const Args &... args) {
   MakeStringInternal(ss, t);
   MakeStringInternal(ss, args...);
 }
 
-template <typename... Args>
-string MakeString(const Args&... args) {
+template <typename... Args> string MakeString(const Args &... args) {
   std::stringstream ss;
   MakeStringInternal(ss, args...);
   return string(ss.str());
 }
 
-template <>
-string MakeString(const string& s);
+template <> string MakeString(const string &s);
 
-string MakeString(const char* chr);
+string MakeString(const char *chr);
 
 // remove all instances of pred in a string
 
-template <typename T, typename P>
-T RemoveIf(T beg, T end, P pred) {
+template <typename T, typename P> T RemoveIf(T beg, T end, P pred) {
   T dest = beg;
   for (T itr = beg; itr != end; ++itr)
-    if (!pred(*itr)) *(dest++) = *itr;
+    if (!pred(*itr))
+      *(dest++) = *itr;
   return dest;
 }
 
 // checks if full_string ends with ending
-bool HasEnding(const string& full_string, const string& ending);
+bool HasEnding(const string &full_string, const string &ending);
 
 // if a string begins with seq, removes seq in-place and returns true
 // otherwise returns false and does not modify arg
-bool Consume(string& arg, const string& seq);
+bool Consume(string &arg, const string &seq);
 
 // if a string begins with c, removes c in-place and returns true
 // otherwise returns false and does not modify arg
-bool Consume(string& arg, char c);
+bool Consume(string &arg, char c);
 
 // returns a string containing every character up till the next occurence of sec
-string SplitOnNextOccurence(string& arg, const string& seq);
+string SplitOnNextOccurence(string &arg, const string &seq);
 
 // parses a comma separated string and casts to type T,
 // returning a set
@@ -99,34 +97,38 @@ Container ParseArgString(string str) {
       str.erase(0, pos + 1);
     }
   }
-  if (CanCast<T>(str)) ret.insert(CastTo<T>(str));
+  if (CanCast<T>(str))
+    ret.insert(CastTo<T>(str));
 
   return ret;
 }
 
 template <typename T>
-std::vector<T> ParseArgStringToVec(string str) {
+std::vector<T> ParseArgStringToVec(string str, string sep = ",",
+                                   bool ignore_spaces = true) {
   std::vector<T> ret;
 
   // remove all spaces
-  str.erase(::sct::RemoveIf(str.begin(), str.end(), ::isspace), str.end());
+  if (ignore_spaces)
+    str.erase(::sct::RemoveIf(str.begin(), str.end(), ::isspace), str.end());
 
   string token;
-  while (str.find(",") != string::npos) {
-    size_t pos = str.find(",");
+  while (str.find(sep) != string::npos) {
+    size_t pos = str.find(sep);
     token = str.substr(0, pos);
     if (CanCast<T>(token)) {
       ret.push_back(CastTo<T>(token));
-      str.erase(0, pos + 1);
+      str.erase(0, pos + sep.size());
     }
   }
-  if (CanCast<T>(str)) ret.push_back(CastTo<T>(str));
+  if (CanCast<T>(str))
+    ret.push_back(CastTo<T>(str));
 
   return ret;
 }
 
 template <class Container>
-string Concat(const Container& c, const string& delim) {
+string Concat(const Container &c, const string &delim) {
   std::stringstream ss;
   int count = c.size() - 1;
   for (auto i = c.begin(); i != c.end(); ++i, --count) {
@@ -135,6 +137,6 @@ string Concat(const Container& c, const string& delim) {
   return string(ss.str());
 }
 
-}  // namespace sct
+} // namespace sct
 
-#endif  // SCT_LIB_STRING_STRING_UTIL_H
+#endif // SCT_LIB_STRING_STRING_UTIL_H
