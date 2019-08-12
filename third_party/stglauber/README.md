2e0a327c085e13e93ee5c9aee71c0926f2abf7fa
Nick Elsey <nicholas.elsey@gmail.com>
2019-08-12 00:02:50 -0400

    Revert "adding stglauber"
    This reverts commit a1c776f2e14cb5e45cf09cfa21660a9b9568bbb5.

--- third_party/stglauber/README.md
+++
@@ -1,1 +0,0 @@
-A cmake project of the STAR StFastGlauber library, stripped of any extra STAR Library dependencies. Used for testing the consistency of sct
--- third_party/stglauber/cmake/SysCheck.cmake
+++
@@ -1,5 +0,0 @@
-# as of now, this isn't being used... probably won't be used unless
-# optimizations are needed for specific platforms
-
-include(CheckCXXSourceCompiles)
-include(CMakePushCheckState)
